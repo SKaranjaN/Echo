3d60216e3211c6d0ad66b6e7f7b580e0b3fe94cd
stephen <skaranja654@gmail.com>
2023-08-25 18:48:33 +0300

    wavy png

--- client/src/components/ViewText.js
+++ client/src/components/ViewText.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import '../styles/ViewText.css'
+import '../styles/ViewText.css';
+import wavyImage from '../images/wavy.png';
 
 function ViewText() {
-    return(
+    return (
         <div className="view-text">
             <h3>Click on the Play button below to start playing</h3>
+            <img src={wavyImage} alt="Wavy" className="wavy-image" />
         </div>
-    )
+    );
 }
+
 export default ViewText;
