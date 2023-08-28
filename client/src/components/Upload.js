5c5a33af7dcbee8307d26206688a4a1968ae0fba
stephen <skaranja654@gmail.com>
2023-08-28 15:27:03 +0300

    cloudinary upload works

--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -1,55 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
+import { Image } from 'cloudinary-react';
+import { CloudinaryContext, Transformation, Video } from 'cloudinary-react';
 import "../styles/Upload.css";
 import plusImage from "../images/Plus.png";
 
 function Upload() {
-  const [file, setFile] = useState(null);
-  const [url, setUrl] = useState("");
-  const [uploadProgress, setUploadProgress] = useState(0);
-  const [uploadComplete, setUploadComplete] = useState(false);
+  const cloudName = "dyahkvt1m";
+  const uploadPreset = "ml_default";
 
-  function handleFileChange(event) {
-    const selectedFile = event.target.files[0];
-    setFile(selectedFile);
-  }
-
-  async function uploadToCloudinary() {
-    if (!file) return;
-
+  function handleFileUpload(event) {
+    const file = event.target.files[0];
     const formData = new FormData();
-    formData.append("file", file, file.name);
-    formData.append(
-      "resource_type",
-      file.type.includes("audio") ? "audio" : "video"
-    );
-
-    try {
-      const response = await fetch("http://localhost:5000/upload", {
-        method: "POST",
-        body: formData,
-        onUploadProgress: (progressEvent) => {
-          const progress = Math.round(
-            (progressEvent.loaded / progressEvent.total) * 100
-          );
-          setUploadProgress(progress);
-        },
+    formData.append("file", file);
+    formData.append("upload_preset", uploadPreset);
+  
+    fetch(`https://api.cloudinary.com/v1_1/${cloudName}/upload`, {
+      method: "POST",
+      body: formData,
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data); 
+      })
+      .catch(error => {
+        console.error("Error uploading to Cloudinary:", error);
       });
-
-      if (response.ok) {
-        const data = await response.json();
-        setUrl(data.secure_url);
-        setUploadComplete(true);
-        setUploadProgress(100);
-        console.log("Uploaded URL:", data.secure_url);
-      } else {
-        console.error(
-          "Error uploading file to Cloudinary. Status:",
-          response.status
-        );
-      }
-    } catch (error) {
-      console.error("Error uploading file to Cloudinary:", error);
-    }
   }
 
   return (
@@ -63,34 +38,8 @@ function Upload() {
         className="file-input"
         type="file"
         accept="audio/*, video/*"
-        onChange={handleFileChange}
+        onChange={handleFileUpload}
       />
-
-      {uploadProgress > 0 && uploadProgress < 100 && (
-        <div className="progress-bar" style={{ width: `${uploadProgress}%` }}>
-          {uploadProgress}% Uploaded
-        </div>
-      )}
-
-      {uploadComplete && (
-        <div className="upload-complete-message">Upload Complete</div>
-      )}
-
-      {url && (
-        <div>
-          {file.type.includes("audio") ? (
-            <audio controls>
-              <source src={url} type="audio/mp3" />
-              Your browser does not support the audio element.
-            </audio>
-          ) : (
-            <video controls>
-              <source src={url} type="video/mp4" />
-              Your browser does not support the video element.
-            </video>
-          )}
-        </div>
-      )}
     </div>
   );
 }
