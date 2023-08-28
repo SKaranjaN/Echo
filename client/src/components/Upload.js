09e7236e5496d7804c9060bb261bc8ca61b37995
stephen <skaranja654@gmail.com>
2023-08-28 15:42:29 +0300

    upload tracker

--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -1,30 +1,40 @@
-import React from "react";
-import { Image } from 'cloudinary-react';
-import { CloudinaryContext, Transformation, Video } from 'cloudinary-react';
+import React, { useState } from "react";
 import "../styles/Upload.css";
 import plusImage from "../images/Plus.png";
 
 function Upload() {
   const cloudName = "dyahkvt1m";
   const uploadPreset = "ml_default";
+  const [uploadProgress, setUploadProgress] = useState(0);
 
   function handleFileUpload(event) {
     const file = event.target.files[0];
     const formData = new FormData();
     formData.append("file", file);
     formData.append("upload_preset", uploadPreset);
-  
-    fetch(`https://api.cloudinary.com/v1_1/${cloudName}/upload`, {
-      method: "POST",
-      body: formData,
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data); 
-      })
-      .catch(error => {
-        console.error("Error uploading to Cloudinary:", error);
-      });
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.upload.onprogress = event => {
+      if (event.lengthComputable) {
+        const percentComplete = (event.loaded / event.total) * 100;
+        setUploadProgress(percentComplete);
+      }
+    };
+
+    xhr.open("POST", `https://api.cloudinary.com/v1_1/${cloudName}/upload`, true);
+
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const data = JSON.parse(xhr.responseText);
+        console.log("Upload complete:", data);
+        setUploadProgress(0); // Reset progress after completion
+      } else {
+        console.error("Upload failed:", xhr.responseText);
+      }
+    };
+
+    xhr.send(formData);
   }
 
   return (
@@ -40,6 +50,9 @@ function Upload() {
         accept="audio/*, video/*"
         onChange={handleFileUpload}
       />
+      <div className="progress-text">
+        {uploadProgress === 100 ? "Upload Complete" : `${uploadProgress.toFixed(2)}%`}
+      </div>
     </div>
   );
 }
