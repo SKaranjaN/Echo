da0c46c590eba6274ff1041d9f63a9d63844b9bd
stephen <skaranja654@gmail.com>
2023-08-30 11:47:57 +0300

    patch

--- client/src/components/ViewText.js
+++ client/src/components/ViewText.js
@@ -16,19 +16,9 @@ function ViewText({ transcriptionText, onEdit, onSave }) {
     onSave(editedText);
   };
 
-  const handleBlur = () => {
-    if (isEditing) {
-      handleSaveClick();
-    }
-  };
-
   const handleContentChange = (event) => {
     setEditedText(event.target.textContent);
   };
-
-  useEffect(() => {
-    // console.log(`ViewText (editedText): ${editedText}`);
-  }, [editedText]);
 
   return (
     <div className="view-text">
@@ -36,30 +26,30 @@ function ViewText({ transcriptionText, onEdit, onSave }) {
       <p>Click on the play button below</p>
       <div className="text-result scrollable">
         {isEditing ? (
-          <>
-            <div
-              contentEditable={true}
-              suppressContentEditableWarning={true}
-              onBlur={handleBlur}
-              onInput={handleContentChange}
-              className="edit-text"
-            >
-              {editedText}
-            </div>
-            <button onClick={handleSaveClick}>Save</button>
-          </>
+          <div
+            contentEditable={true}
+            suppressContentEditableWarning={true}
+            onInput={handleContentChange}
+            className="edit-text"
+          >
+            {editedText}
+          </div>
         ) : (
-          <>
-            <div>{transcriptionText}</div>
-            <span
-              className="edit-icon"
-              role="img"
-              aria-label="Edit"
-              onClick={handleEditClick}
-            >
-              ✏️
-            </span>
-          </>
+          <div>{transcriptionText}</div>
+        )}
+      </div>
+      <div className="edit-button-container">
+        {isEditing ? (
+          <button onClick={handleSaveClick}>Save</button>
+        ) : (
+          <span
+            className="edit-icon"
+            role="img"
+            aria-label="Edit"
+            onClick={handleEditClick}
+          >
+            ✏️
+          </span>
         )}
       </div>
       <img src={wavyImage} alt="Wavy" className="wavy-image" />
